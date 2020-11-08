4946ac737e7d2e1508d2c3f89eda6596858d5919
Mirco Poretti <mircoporetti@gmail.com>
2020-11-08 18:59:33 +0100

    Single roll and many rolls

--- bowling-game/src/main/java/me/mircoporetti/tddwithjava/bowling/Game.java
+++ bowling-game/src/main/java/me/mircoporetti/tddwithjava/bowling/Game.java
@@ -2,4 +2,13 @@ package me.mircoporetti.tddwithjava.bowling;
 
 public class Game {
 
+    private int score = 0;
+
+    public void roll(int pins) {
+        this.score += pins;
+    }
+
+    public int score() {
+        return score;
+    }
 }
--- bowling-game/src/test/java/me/mircoporetti/tddwithjava/bowling/GameTest.java
+++ bowling-game/src/test/java/me/mircoporetti/tddwithjava/bowling/GameTest.java
@@ -1,5 +1,6 @@
 package me.mircoporetti.tddwithjava.bowling;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -7,8 +8,45 @@ import static org.hamcrest.Matchers.is;
 
 class GameTest {
 
-    @Test
-    void name() {
-        assertThat(true, is(true));
+    private Game underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new Game();
     }
+
+    private void rollMany(int rolls, int pins) {
+        for (int i = 0; i < rolls; i++)
+            underTest.roll(pins);
+    }
+
+    @Test
+    void singleRollWithNoPins() {
+        underTest.roll(0);
+
+        assertThat(underTest.score(), is(0));
+    }
+
+    @Test
+    void singleRollWithOnePin() {
+        underTest.roll(1);
+
+        assertThat(underTest.score(), is(1));
+    }
+
+    @Test
+    void rollManyWithNoPins() {
+        rollMany(20,0);
+
+        assertThat(underTest.score(), is(0));
+    }
+
+    @Test
+    void rollManyWithAllOnePin() {
+        rollMany(20,1);
+
+        assertThat(underTest.score(), is(20));
+    }
+
+
 }
