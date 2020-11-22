b309a055bc7d18137237eea7a962ea866a8b3590
Mirco Poretti <mircoporetti@gmail.com>
2020-11-22 15:45:03 +0100

    Refactoring production code

--- bowling-game/src/main/java/me/mircoporetti/tddwithjava/bowling/game/Game.java
+++ bowling-game/src/main/java/me/mircoporetti/tddwithjava/bowling/game/Game.java
@@ -7,77 +7,114 @@ import java.util.List;
 
 public class Game {
 
-    private final int maxFrames = 10;
+    private final int MAX_FRAMES = 10;
+    private int score = 0;
 
     private final List<Frame> frames = new ArrayList<>();
 
     public void roll(int pins) {
-        Frame currentFrame;
-        if(moreFramesAvailable()) {
+        if(moreRollsAvailable()) {
             if (isTheFirstFrameOrIsThePreviousClosed()) {
-                if(frames.size() == 9){
-                    currentFrame = new LastFrame();
-                }else{
-                    currentFrame = new Frame();
-                }
-                currentFrame.firstRoll = pins;
-                if (currentFrame.isAStrike())
-                    if (!(currentFrame instanceof LastFrame)) {
-                        currentFrame.closed = true;
-                    }
-                frames.add(currentFrame);
+                rollInNewFrame(pins);
             } else {
-                currentFrame = frames.get(frames.size() - 1);
-                if (currentFrame instanceof LastFrame){
-                    if(((LastFrame) currentFrame).extraRollAvailable) {
-                        ((LastFrame) currentFrame).thirdRoll = pins;
-                        currentFrame.closed = true;
-                    }else{
-                        currentFrame.secondRoll = pins;
-                        if(currentFrame.isASpare() || currentFrame.isAStrike())
-                            ((LastFrame) currentFrame).extraRollAvailable = true;
-                        else currentFrame.closed = true;
-                    }
-                }else{
-                    currentFrame.secondRoll = pins;
-                    currentFrame.closed = true;
-                }
+                rollInExistingFrame(pins);
             }
         }else{
             throw new TooManyGamesException("Ehi man too many rolls, the game is finished!");
         }
     }
 
+    private boolean moreRollsAvailable() {
+        return frames.size() < MAX_FRAMES || (frames.size() == 10 && !frames.get(frames.size() - 1).closed);
+    }
+
+    private boolean isTheFirstFrameOrIsThePreviousClosed() {
+        return frames.isEmpty() || frames.get(frames.size() - 1).closed;
+    }
+
+    private void rollInNewFrame(int pins) {
+        Frame currentFrame;
+        currentFrame = buildFrame();
+        currentFrame.firstRoll = pins;
+        if (currentFrame.isAStrike())
+            closeFrameIfNotLast(currentFrame);
+        frames.add(currentFrame);
+    }
+
+    private Frame buildFrame() {
+        Frame currentFrame;
+        if(frames.size() == MAX_FRAMES - 1){
+            currentFrame = new LastFrame();
+        }else{
+            currentFrame = new Frame();
+        }
+        return currentFrame;
+    }
+
+    private void closeFrameIfNotLast(Frame currentFrame) {
+        if (!isTheLastFrame(currentFrame)) {
+            currentFrame.closed = true;
+        }
+    }
+
+    private void rollInExistingFrame(int pins) {
+        Frame currentFrame = frames.get(frames.size() - 1);
+        if (isTheLastFrame(currentFrame)){
+            if(((LastFrame) currentFrame).extraRollAvailable) {
+                ((LastFrame) currentFrame).thirdRoll = pins;
+                currentFrame.closed = true;
+            }else{
+                currentFrame.secondRoll = pins;
+                if(currentFrame.isASpare() || currentFrame.isAStrike())
+                    ((LastFrame) currentFrame).extraRollAvailable = true;
+                else currentFrame.closed = true;
+            }
+        }else{
+            currentFrame.secondRoll = pins;
+            currentFrame.closed = true;
+        }
+    }
+
     public int score(){
-        int score = 0;
         for (int i = 0; i < frames.size(); i++) {
             Frame frame = frames.get(i);
-            if(!(frame instanceof LastFrame)) {
-                if (frame.isASpare()) {
-                    score += frame.firstRoll + frame.secondRoll + frames.get(i + 1).firstRoll;
-                } else if (frame.isAStrike()) {
-                    score += frame.firstRoll + frames.get(i + 1).firstRoll;
-                    if (frames.get(i + 1).firstRoll == 10 && !(frames.get(i + 1) instanceof LastFrame)){
-                        score += frames.get(i + 2).firstRoll;
-                    }else{
-                        score += frames.get(i + 1).secondRoll;
-                    }
-                } else {
-                    score += frame.firstRoll + frame.secondRoll;
-                }
+            if(!(isTheLastFrame(frame))) {
+                scoreRegularFrame(i, frame);
             }else{
-                score += frame.firstRoll + frame.secondRoll + ((LastFrame) frame).thirdRoll;
+                scoreLastFrame(frame);
             }
         }
         return score;
     }
 
-    private boolean moreFramesAvailable() {
-        return frames.size() < maxFrames || (frames.size() == 10 && !frames.get(frames.size() - 1).closed);
+    private void scoreRegularFrame(int currentFrameIndex, Frame frame) {
+        if (frame.isASpare()) {
+            Frame nextFrame = frames.get(currentFrameIndex + 1);
+            score += frame.firstRoll + frame.secondRoll + nextFrame.firstRoll;
+        } else if (frame.isAStrike()) {
+            Frame nextFrame = frames.get(currentFrameIndex + 1);
+            score += frame.firstRoll + nextFrame.firstRoll;
+            if (isAStrikeAndNotTheLastFrame(nextFrame)){
+                Frame nextTwo = frames.get(currentFrameIndex + 2);
+                score += nextTwo.firstRoll;
+            }else{
+                score += nextFrame.secondRoll;
+            }
+        } else {
+            score += frame.firstRoll + frame.secondRoll;
+        }
     }
 
-    private boolean isTheFirstFrameOrIsThePreviousClosed() {
-        return frames.isEmpty() || frames.get(frames.size() - 1).closed;
+    private boolean isAStrikeAndNotTheLastFrame(Frame frame) {
+        return frame.firstRoll == 10 && !(isTheLastFrame(frame));
+    }
+
+    private boolean isTheLastFrame(Frame currentFrame) {
+        return currentFrame instanceof LastFrame;
+    }
+
+    private void scoreLastFrame(Frame frame) {
+        score += frame.firstRoll + frame.secondRoll + ((LastFrame) frame).thirdRoll;
     }
 }
 
